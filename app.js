2733c41ff555f5cb577d127838a69844315c172d
Tristan Wang <aawang99@gmail.com>
2021-07-19 14:23:02 -0700

    feat: add heroku init

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const toDate = require('./tools/handlebarsHelpers')
 require('./config/mongoose')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({
   defaultLayout: 'main',
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,22 +4,24 @@ const Category = require('../../models/category')
 const Record = require('../../models/record')
 
 router.get('/', (req, res) => {
+  const category = req.query.category
+  const filter = {}
+  if (category) { filter.category = category }
+
   const categories = []
 
   Category.find()
     .lean()
-    .then(category => {
-      categories.push(...category)
-    })
+    .then(category => categories.push(...category))
     .catch(error => console.log(error))
 
-  Record.find()
+  Record.find(filter)
     .populate('category')
     .lean()
     .then((records) => {
       let totalAmount = 0
       records.forEach(record => totalAmount += record.amount)
-      res.render('index', { categories, records, totalAmount })
+      res.render('index', { categories, category, records, totalAmount })
     })
     .catch(error => console.log(error))
 })
