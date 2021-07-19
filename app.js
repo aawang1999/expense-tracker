3db693e28c18096d7f292725bf1191e2812e9772
Tristan Wang <aawang99@gmail.com>
2021-07-19 13:08:54 -0700

    modify: add method override

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const routes = require('./routes')
 const toDate = require('./tools/handlebarsHelpers')
 require('./config/mongoose')
@@ -16,6 +17,7 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(routes)
 app.use(express.static('public'))
 
