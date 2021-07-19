fdf94c769cef582f043ae1bdc82815afe4ade2cc
Tristan Wang <aawang99@gmail.com>
2021-07-19 13:25:21 -0700

    feat: add edit and delete functions

--- public/javascripts/records.js
+++ public/javascripts/records.js
@@ -1,4 +1,6 @@
+const category = document.querySelector('#category')
 const form = document.querySelector('.needs-validation')
+category.value = category.dataset.value
 
 form.addEventListener('submit', function onFormSubmitted(event) {
   if (!form.checkValidity()) {
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -2,16 +2,52 @@ const express = require('express')
 const router = express.Router()
 const Category = require('../../models/category')
 const Record = require('../../models/record')
+const categories = []
+
+Category.find()
+  .lean()
+  .then(category => categories.push(...category))
+  .catch(error => console.log(error))
+
+// Create
 
 router.get('/new', (req, res) => {
-  Category.find()
-    .lean()
-    .then(categories => res.render('new', { categories }))
-    .catch(error => console.log(error))
+  res.render('new', { categories })
 })
 
 router.post('/', (req, res) => {
   Record.create(req.body)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// Edit
+
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then(record => res.render('edit', { categories, record }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => {
+      Object.assign(record, req.body)
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// Delete
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
