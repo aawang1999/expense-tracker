181350470478ac179459d77552c03551ef44c603
Tristan Wang <aawang99@gmail.com>
2021-07-19 11:13:01 -0700

    feat: add handlebars helpers and update app.js

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
+const toDate = require('./tools/handlebarsHelpers')
+require('./config/mongoose')
 
 const app = express()
 const PORT = 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: { toDate }
+}))
 
 app.set('view engine', 'hbs')
 
 app.use(routes)
+app.use(express.static('public'))
 
 app.listen(PORT, () => {
   console.log(`Express is running on port ${PORT}.`)
