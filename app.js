b7a32bbd191dc179985c22b7cb0219a8c6ff3daf
Tristan Wang <aawang99@gmail.com>
2021-07-19 10:22:07 -0700

    feat: initialize project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const routes = require('./routes')
+
+const app = express()
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+
+app.set('view engine', 'hbs')
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`Express is running on port ${PORT}.`)
+})
